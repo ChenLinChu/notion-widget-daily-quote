831adca91bf36f3efbd5ea3673a5b36a6c5295f1
ChenLin Chu <chenlinchu@ChenLins-Macbook.local>
2022-03-21 20:51:57 +0800

    [開發] 樣式調整 + 增加資料

--- quoteData.js
+++ quoteData.js
@@ -1,7 +1,55 @@
 const quoteData = [
     {
-        content: 'A great man doesn’t seek to lead. He’s called to it, and he answers.',
-        author: 'Duke Leto Atreides'
+        content: "A great man doesn’t seek to lead. He’s called to it, and he answers.",
+        author: "Duke Leto Atreide"
+    },
+    {
+        content: "When something is important enough, you do it even if the odds are not in your favor.",
+        author: "Elon Musk",
+    },
+    {
+        content: "I think it is possible for ordinary people to choose to be extraordinary.",
+        author: "Elon Musk"
+    },
+    {
+        content: "Failure is an option here. If things are not failing, you are not innovating enough.",
+        author: "Elon Musk"
+    },
+    {
+        content: "When you struggle with a problem, that's when you understand it.",
+        author: "Elon Musk"
+    },
+    {
+        content: "Life is too short for long-term grudges.",
+        author: "Elon Musk"
+    },
+    {
+        content: "Weak people revenge. Strong people forgive. Intelligent People Ignore.",
+        author: "Albert Einstein"
+    },
+    {
+        content: "A person who never made a mistake never tried anything new.",
+        author: "Albert Einstein"
+    },
+    {
+        content: "Time you enjoy wasting, was not wasted.",
+        author: "John Lennon"
+    },
+    {
+        content: "No matter what people tell you, words and ideas can change the world.",
+        author: "Robin Williams"
+    },
+    {
+        content: "Don’t let the noise of others’ opinions drown out your own inner voice.",
+        author: "Steve Jobs"
+    },
+    {
+        content: "A mind is like a parachute. It doesn't work if it is not open.",
+        author: "Frank Zappa"
+    },
+    {
+        content: "When you want something, all the universe conspires to help you achieve it.",
+        author: "Paulo Coelh"
     }
 ]
 
