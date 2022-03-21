30dab26605ea2014f17d97b7b905c553ec7c6f4d
ChenLin Chu <chenlinchu@ChenLins-Macbook.local>
2022-03-21 19:51:43 +0800

    [初始] 初始化

---
+++ quoteData.js
@@ -0,0 +1,8 @@
+const quoteData = [
+    {
+        content: 'A great man doesn’t seek to lead. He’s called to it, and he answers.',
+        author: 'Duke Leto Atreides'
+    }
+]
+
+export default quoteData;
